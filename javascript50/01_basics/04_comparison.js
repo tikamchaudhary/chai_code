b680c2e7a99e377ec2819f338cb1e0d80f2dd317
Tikam Chaudhary <117741001+tikamchaudhary172@users.noreply.github.com>
2023-09-28 01:52:30 +0000

    comparison

---
+++ javascript50/01_basics/04_comparison.js
@@ -0,0 +1,35 @@
+// console.log(2 == 1); // false
+// console.log(2 != 1);  // true
+// console.log(2 > 1);   // true
+// console.log(2 >= 1);   // true
+// console.log(2 < 1);   // false
+// console.log(2 <= 1);  // false
+
+
+// console.log("2">1);  // true
+// console.log("02">1); // true
+
+/**************************** null ***************/
+// console.log(null != 0); // true
+// console.log(null == 0); //false
+// console.log(null > 0); // false
+// console.log(null < 0); // false
+// console.log(null >= 0); // true
+// console.log(null <= 0); // true
+
+/*********** undefined ********************** */
+
+// console.log(undefined != 0); // true
+// console.log(undefined == 0);  // false
+// console.log(undefined > 0);  // false
+// console.log(undefined >= 0); // false
+// console.log(undefined < 0); // false
+// console.log(undefined <= 0); // false
+
+/**************** strict equal (===) *******************/
+
+// console.log("2"==2); // true
+// console.log("2"===2); // false
+
+console.log(null==undefined); //true
+console.log(null===undefined); // false
