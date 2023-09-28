9ddecb7ed8a01781523e4f857301ca45f4af0d5a
Tikam Chaudhary <117741001+tikamchaudhary172@users.noreply.github.com>
2023-09-28 08:36:22 +0000

    memory in js

--- javascript50/01_basics/05_datatypes-summary.js
+++ javascript50/01_basics/05_datatypes-summary.js
@@ -36,8 +36,27 @@ const login = function () {
     console.log("Hell user");
 }
 
-console.log(typeof num);
-console.log(typeof user);
-console.log(typeof login);
+// console.log(typeof num);
+// console.log(typeof user);
+// console.log(typeof login);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+/********** Memory in javascript ***********/
+
+//1. Stack memory:- primitive datatypes
+//2. Heap memory:- non-primitive datatypes
+
+let num1 = 45;
+let num2 = num1;
+num2 = 50;
+
+const user1 = { name: "Tikam", age: 30 };
+const user2 = user1;
+user2.name = "Shivam";
+
+console.log("num1: ", num1);
+console.log("num2: ", num2);
+console.log("user1: ", user1);
+console.log("user2: ", user2);
