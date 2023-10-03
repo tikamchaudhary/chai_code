17c6624004187cea6d5498f70695e27aeb864f2a
Tikam Chaudhary <117741001+tikamchaudhary172@users.noreply.github.com>
2023-10-03 11:44:37 +0000

    array part1

--- javascript50/02_basics/01_array.js
+++ javascript50/02_basics/01_array.js
@@ -6,11 +6,14 @@ const fruits = new Array('apple', 'orange', 'mango', 'orange', 'banana', 'orange
 
 const names = "Tikam,Shivam,Aman,Rahul,Vishal".split(',');//created using String.prototype.split() 
 
-// console.log('numbers: ', numbers, " length: ", numbers.length);
-// console.log('numbers1: ', numbers1,);
-// console.log('fruits: ', fruits);
-// console.log('names: ',names);
+console.log('numbers: ', numbers, " length: ", numbers.length);
+console.log('numbers1: ', numbers1,);
+console.log('fruits: ', fruits);
+console.log('names: ',names);
 
+/************* update array elements by index *********************/
+// numbers1[2]='five';
+// console.log(numbers1);
 
 /*********** Create a string from an array ****************************/
 // console.log(numbers.toString(),typeof(numbers.toString())); // 2,4,6,8,10 string
@@ -113,6 +116,9 @@ const newLength = fruits1.push('banaba', 'guava');
 // console.log('fruits1:',fruits1);
 // console.log('new length:',newLength);
 
+fruits1[fruits1.length] = "ORANGE";
+// console.log('fruits1:',fruits1);
+
 
 /******************** Remove the last item from an array **************************************/
 // .pop() ==> Removes the last element from an array and returns it. If the array is empty, undefined is returned and the array is not modified.
@@ -171,5 +177,240 @@ const new_myArray = myArray.filter((_, index) => index >= 3); // remove the firs
 // array.slice(startIndex, endIndex); // note:- endIndex is not include.
 const otherNums = [1, 2, 3, 4, 5, 6];
 const new_otherNums = otherNums.slice(3); // remove the first 3 items
-console.log('otherNums:', otherNums);
-console.log('new otherNums:', new_otherNums);
+// console.log('otherNums:', otherNums);
+// console.log('new otherNums:', new_otherNums);
+
+/************** Add a new first item to an array ******************/
+// array.unshift() => Inserts new elements at the start of an array, and returns the new length of the array.
+const numbers5 = [4, 5, 6];
+// console.log('number5:', numbers5, 'length:', numbers5.length);
+const new_length = numbers5.unshift(100, 200);
+// console.log('number5:', numbers5, "new_length", new_length);
+
+/****************** Remove a single item by index *****************************/
+
+// array.splice(startIndex, deleteCount, addItem);
+const fruits3 = ['mango', 'apple', "Banana", 'orange'];
+// console.log('fruits3:', fruits3);
+//to remove the string "Banana" from the fruits array — by specifying the index position of "Banana".
+const bananaIndex = fruits3.indexOf('Banana');
+const removed__item = fruits3.splice(bananaIndex, 1);
+// console.log('fruits3:', fruits3);
+// console.log('removed item:',removed__item);
+
+/************** Remove multiple items by index ***************************/
+
+// array.splice(startIndex, deleteCount, addItem);
+const fruits4 = ["Apple", "Banana", "Strawberry", "Mango"];
+// console.log('fruits4:', fruits4);
+const remove_items = fruits4.splice(1, 2);
+// console.log('fruits4:', fruits4);
+// console.log('remove_items:', remove_items);
+
+/******************* Replace multiple items in an array************************/
+const fruits5 = ["Apple", "Banana", "Strawberry", "Mango"];
+// console.log('fruits5:', fruits5);
+const remove__items = fruits5.splice(-2, 2); // replace the last 2 items
+// console.log('fruits5:', fruits5);
+// console.log('remove__items:', remove__items);
+
+/******************* insert/replace new  items in an array************************/
+const fruits6 = ['mango', 'banana', 'apple'];
+// console.log('fruits6:',fruits6);
+const addItems = fruits6.splice(1, 0, 'papaya', 'orange'); //  insert items
+
+// console.log('fruits6:',fruits6);
+// console.log('add items:',addItems);
+
+const remove_Items = fruits6.splice(1, 2, 'GUAVA', 'GRAPES'); // replace items
+// console.log('fruits6:',fruits6);
+// console.log('remove_Items:',remove_Items);
+
+/************************ Iterate over an array **********************************/
+//  for loop
+const users = ['Tikam', 'Ajay', 'Shivam', 'Rahul'];
+
+for (let i = 0; i < users.length; i++) {
+    // console.log(users[i]);
+}
+
+// for...of loop
+for (const user of users) {
+    // console.log(user);
+}
+
+
+/****************** Call a function on each element in an array ******************/
+/* 
+array.forEach(callback[, thisArg]);
+(or)
+array.forEach(callback(currentValue[, index[, array]])[, thisArg]);
+note:- forEach() method return always 'undefined'.
+*/
+
+const userNames = ['Tikam', 'Ajay', 'Shivam', 'Rahul'];
+userNames.forEach((currentName, index, array) => {
+
+    // index === 0 && console.log('userNames:', array);
+    // console.log(`index ${index}: ${currentName}`);
+
+});
+
+// use case for the thisArg parameter:-
+
+//1. Preserving the Context:-
+const myObject = {
+    value: 2,
+    process: function (item) {
+        // console.log(item * this.value);
+    }
+};
+
+const array = [1, 2, 3];
+array.forEach(myObject.process, myObject);
+
+// 2.Working with Asynchronous Code:-
+const dataProcessor = {
+    data: [1, 2, 3],
+    processAsync: function () {
+        // 'this' is available here.
+        this.data.forEach(function (item) {
+            // 'this' is availavle here.
+            const _this = this;
+            setTimeout(function () {
+                // console.log(item);
+                // item === 1 && console.log(_this.data);
+            }, 100);
+        }, this);
+
+    }
+};
+
+dataProcessor.processAsync();
+
+// 3.Using Functions with Specific Contexts:-
+const userObj = { name: 'Tikam Singh', age: 30 }
+function printValue(_, index) {
+    // console.log(index, this.name);
+}
+
+const values = [10, 20, 30];
+values.forEach(printValue, userObj);
+
+// other use case:-
+const num = [1, 2, 3];
+num.forEach(function () {
+    // console.log(this);
+}, num);
+
+/*************************** Merge multiple arrays together**************************/
+// .concat()
+const arr1 = [0, 1, 2];
+const arr2 = [3, 4];
+const arr3 = [5];
+const arrMain = arr1.concat(arr2, arr3, 'Tikam');
+// console.log('arr1', arr1);
+// console.log('arr2', arr2);
+// console.log('arr3', arr2);
+// console.log('arrMain', arrMain);
+
+// spread operator (...)
+// console.log([...arr1,...arr2,...arr3,"mango"]);
+
+// .push()
+const array1 = [1, 2, 3];
+const array2 = [4, 5];
+const array3 = [];
+for (const element of array1) {
+    array3.push(element);
+}
+for (const element of array2) {
+    array3.push(element);
+}
+
+// console.log('array1:', array1);
+// console.log('array2', array2);
+// console.log('array3', array3);
+
+
+/*********************** Copy an array ***************************************/
+const languages = ['html', 'css', 'javascript', 'python'];
+
+// 1.spread operator (...)
+const copy_languages1 = [...languages];
+
+//2. slice()
+const copy_languages2 = languages.slice();
+const copy_slice = languages.slice(1, 3); // last index is not includes
+
+//3. Array.from() => Creates an array from an iterable object.
+const copy_languages3 = Array.from(languages);
+
+//4. concat()
+const copy_languages4 = [].concat(languages);
+
+
+// console.log('languages:', languages);
+// console.log('copy_languages1:', copy_languages1);
+// console.log('copy_languages2:', copy_languages2);
+// console.log('copy_slice:', copy_slice);
+
+// console.log('copy_languages3:', copy_languages3);
+// console.log('copy_languages4:', copy_languages4);
+
+/*
+All built-in array-copy operations (spread syntax, Array.from(), Array.prototype.slice(),
+and Array.prototype.concat()) create shallow copies. 
+If you instead want a deep copy of an array,
+you can use JSON.stringify() to convert the array to a JSON string, 
+and then JSON.parse() to convert the string back into a new array that's completely independent from the original array.
+
+const arrayDeepCopy = JSON.parse(JSON.stringify(array));
+*/
+
+const main_array = ['Tikam', 'Pavan', ['mango', 'apple']];
+
+const main_arrayAlias = main_array;
+main_arrayAlias.push('red');
+
+const shallow_copy = [...main_array];
+shallow_copy[0] = 'Chaudhary';
+shallow_copy[2][0] = 'orange';
+
+const deep_copy = JSON.parse(JSON.stringify(main_array));
+deep_copy[2].push('guava');
+
+// console.log(main_array === main_arrayAlias);
+// console.log('main array:', main_array);
+// console.log('main_arrayAlias:', main_arrayAlias);
+// console.log('shallow_copy:', shallow_copy);
+// console.log('deep_copy:', deep_copy);
+
+
+/*********************** Creating a two-dimensional array **********************************/
+const board = [
+    ["R", "N", "B", "Q", "K", "B", "N", "R"],
+    ["P", "P", "P", "P", "P", "P", "P", "P"],
+    [" ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " "],
+    ["p", "p", "p", "p", "p", "p", "p", "p"],
+    ["r", "n", "b", "q", "k", "b", "n", "r"],
+];
+
+//   console.log(`${board.join("\n")}\n\n`);
+
+board[4][4] = board[6][4];
+board[6][4] = " ";
+//   console.log(board.join("\n"));
+
+/************ Using an array to tabulate a set of value *****************/
+const tableValues = [];
+for (let x = 0; x < 10; x++) {
+    tableValues.push([2 ** x, 2 * x ** 2]);
+}
+// console.table(tableValues);
+
+/**Creating an array using the result of a match**/
+// RegExp
