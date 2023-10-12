6e7fe0835be2d827d947fd7609e940641ac1d1df
Tikam Chaudhary <117741001+tikamchaudhary172@users.noreply.github.com>
2023-10-12 20:53:09 +0000

    arrow_function_and_this

---
+++ javascript50/03_basics/03_arrow.js
@@ -0,0 +1,61 @@
+const user = {
+    userName: 'Tikam Singh',
+    price: 999,
+    welcomeMessage: function () {
+        console.log(`${this.userName}, welcome to website !`);
+        // console.log(this);
+    }
+}
+
+// user.welcomeMessage();
+user.userName = 'Tikam Chaudhary';
+// user.welcomeMessage();
+
+// console.log(this); // empty object 
+
+/*_________________________________________________________________*/
+
+function chai() {
+    const userName = 'Tikam Singh';
+
+    console.log(`Hello ${this.userName} !`); //Hello undefined !
+    console.log(this); // global object (we are using node)
+}
+// chai()
+
+const chai2 = function () {
+    const userName = 'Tikam Singh';
+
+    console.log(`Hello ${this.userName} !!`); //Hello undefined !!
+    console.log(this); // global object (we are using node);
+}
+// chai2()
+
+const chai3 = () => {
+    const userName = 'Tikam Singh';
+
+    console.log(`Hello ${this.userName} !!!`); //Hello undefined !!!
+    console.log(this); // empty object (we are using arrow function in node);
+}
+// chai3()
+
+/*________________________________________________________________________*/
+
+// const addTwoNum = (num1, num2) => {
+//     // return (num1 + num2);
+//     return num1 + num2;
+// }
+
+
+// const addTwoNum = (num1, num2) => num1 + num2;
+
+
+// const addTwoNum = (num1, num2) => (num1 + num2);
+
+
+const addTwoNum = (num1, num2) => ({ userName: 'Ajay' });
+
+// console.log(addTwoNum(4, 5));
+
+const squar = num => num ** 2;
+console.log(squar(4));
