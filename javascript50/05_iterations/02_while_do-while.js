db440e2442983083da3b089593144be7120ba1b3
Tikam Chaudhary <117741001+tikamchaudhary172@users.noreply.github.com>
2023-10-15 16:50:07 +0000

    while and do-while loop

---
+++ javascript50/05_iterations/02_while_do-while.js
@@ -0,0 +1,23 @@
+/******* while, do-while loop ************/
+
+let index = 0;
+
+while (index <= 10) {
+    // console.log(index);
+    index += 2;
+}
+
+
+const fruits = ['apple', 'mango', 'banana'];
+let i = 0;
+while (i < fruits.length) {
+    // console.log(fruits[i]);
+    i++;
+}
+
+let score = 12;
+
+do {
+    console.log(`score: ${score}`);
+    score++;
+} while (score <= 10);
